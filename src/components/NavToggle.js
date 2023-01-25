508d3af6b03701cf2c0168d31e03f720f62c73b9
Leonardo Tricotti <tricottileo@gmail.com>
2023-01-25 09:01:57 -0300

    commit

--- src/components/NavToggle.js
+++ src/components/NavToggle.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import styles from "../css/navToggle.module.css";
 
 function NavToggle() {
+  const [isOpen, setIsopen] = useState(false);
+
+  function handleClick() {
+    setIsopen(!isOpen);
+  }
+
   return (
     <div className={styles.mobileToggleButton}>
-      <button className={styles.toggleButton}>
+      <button className={styles.toggleButton} onClick={handleClick}>
         <span className={styles.toggleContainer}>
-          <span className={styles.toggleLineOne + styles.isActive}></span>
-          <span className={styles.toggleLineTwo + styles.isActive}></span>
-          <span className={styles.toggleLineThree + styles.isActive}></span>
+          <span
+            className={`${styles.toggleLineOne} ${
+              isOpen ? styles.isActive : ""
+            }`}
+          ></span>
+          <span
+            className={`${styles.toggleLineTwo} ${
+              isOpen ? styles.isActive : ""
+            }`}
+          ></span>
+          <span
+            className={`${styles.toggleLineThree} ${
+              isOpen ? styles.isActive : ""
+            }`}
+          ></span>
         </span>
       </button>
     </div>
