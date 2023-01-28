46a9ca34d04183dfde7cfb393220699dd12016f1
Leonardo Tricotti <tricottileo@gmail.com>
2023-01-28 08:34:18 -0300

    commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,5 @@
+import styles from "../css/spinner.module.css";
+
+function Spinner() {
+  return <div className={styles.ldsDualRing}></div>;
+}
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,12 @@
 import HeaderMobile from "../components/HeaderMobile";
+import Table from "../components/Table";
 import styles from "../css/home.module.css";
 
 function Home() {
   return (
     <main className={styles.tableContainer}>
       <HeaderMobile />
+      <Table />
       <h1>Hello</h1>
     </main>
   );
