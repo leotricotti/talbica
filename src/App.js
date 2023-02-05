45f6c9bd37c25cfd1482562ff8e1f585e7b88a77
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-05 10:54:12 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { DataProvider } from "./contexts/DataContext";
 import Home from "./screens/Home";
-import CardModal from "./screens/CardModal";
 
 function App() {
   return (
     <DataProvider>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/card" element={<CardModal />} />
       </Routes>
     </DataProvider>
   );
