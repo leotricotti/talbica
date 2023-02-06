024f083c5cf4ee68dba59e250a538f3b278044e5
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-06 18:37:27 -0300

    commit

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -1,16 +1,18 @@
 import useCutString from "../customHooks/useCutString";
+import useFormatMass from "../customHooks/useFormatMass";
 import styles from "../css/cardHeader.module.css";
 
 function CardModalHeader({ dataFromApi }) {
   const item = dataFromApi;
   const history = useCutString(item.history);
+  const formatMass = useFormatMass(item.atomicMass);
 
   return (
     <div key={item.atomicNumber} className={styles.cardlHeaderContainer}>
       <span className={styles.atomicNumber}>{item.atomicNumber}</span>
       <span className={styles.symbol}>{item.symbol}</span>
       <span className={styles.name}>{item.name}</span>
-      <span className={styles.atomicMass}>{item.atomicMass}</span>
+      <span className={styles.atomicMass}>{formatMass}</span>
       <p className={styles.description}>{history}</p>
     </div>
   );
