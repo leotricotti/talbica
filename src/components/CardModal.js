a10f7848f05441cb303ec6a775d4fd525a52c220
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-06 14:05:28 -0300

    commit

--- src/components/CardModal.js
+++ src/components/CardModal.js
@@ -1,17 +1,10 @@
-import { useState } from "react";
 import CardModalHeader from "./CardModalHeader";
 import styles from "../css/cardModal.module.css";
 
 function CardModal({ dataFromApi, showModal }) {
-  const [closeModal, setCloseModal] = useState(false);
-
-  function closeCardModal() {
-    setCloseModal(!closeModal);
-  }
-
   return (
     <div className={showModal ? styles.showCard : styles.hideCard}>
-      <CardModalHeader dataFromApi={dataFromApi} handleClick={closeCardModal} />
+      <CardModalHeader dataFromApi={dataFromApi} />
     </div>
   );
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,21 +1,13 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { DataContext } from "../contexts/DataContext";
 import Loader from "../components/Loader";
 import HeaderMobile from "../components/HeaderMobile";
 import Table from "../components/Table";
 import SearcBar from "../components/SearchBar";
 import styles from "../css/home.module.css";
-import CardModal from "../components/CardModal";
 
 function Home() {
   const { dataFromApi, error, isLoading } = useContext(DataContext).value;
-  const [opendCardModal, setOpenCardModal] = useState(false);
-
-  const handleClickCardModal = () => {
-    setOpenCardModal(true);
-  };
-
-  console.log(opendCardModal);
 
   if (isLoading) {
     return <Loader />;
@@ -34,10 +26,6 @@ function Home() {
       <HeaderMobile />
       <SearcBar />
       <Table dataFromApi={dataFromApi} />
-      <CardModal
-        dataFromApi={dataFromApi}
-        showModal={() => handleClickCardModal()}
-      />
     </div>
   );
 }
