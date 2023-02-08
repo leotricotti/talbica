ff509a399cf1adaa33b72d9415ffecf718cb9730
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-08 18:27:06 -0300

    commit

--- src/components/CarBody.js
+++ src/components/CarBody.js
@@ -7,8 +7,27 @@ function CardBody({ dataFromApi }) {
     <div key={item.atomicNumber} className={styles.carBodyContainer}>
       <h3 className={styles.cardBodyTitle}>Summary</h3>
       <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Symbol</span>
+        <span className={styles.propertyTitle}>{item.symbol}</span>
         <span className={styles.propertyValue}>{item.symbol}</span>
+      </div>
+      <div className={styles.propertyItems}>
+        <span className={styles.propertyTitle}>{item.name}</span>
+        <span className={styles.propertyValue}>{item.name}</span>
+      </div>
+      <div className={styles.propertyItems}>
+        <span className={styles.propertyTitle}>Chemical category</span>
+        <span className={styles.propertyValue}>{item.groupBlock}</span>
+      </div>
+      <div className={styles.propertyItems}>
+        <span className={styles.propertyTitle}>Atomic weight</span>
+        <span className={styles.propertyValue}>{item.atomicWeight}</span>
+      </div>
+      <div className={styles.propertyItems}>
+        <span className={styles.propertyTitle}>Atomic mass</span>
+        <span className={styles.propertySymbol}>
+          M <sub>a</sub>
+        </span>
+        <span className={styles.propertyValue}>{item.atomicWeight}</span>
       </div>
     </div>
   );
