3579f5fa6a262ded5040438d9445da13fad5d574
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-10 09:24:50 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { DataProvider } from "./contexts/DataContext";
 import Home from "./screens/Home";
+import SearchBarHelp from "./components/SearchBarHelp";
 
 function App() {
   return (
     <DataProvider>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchBarHelp />} />
       </Routes>
     </DataProvider>
   );
--- src/components/SearchBarSection.js
+++ src/components/SearchBarSection.js
@@ -1,12 +1,10 @@
 import SearcBar from "./SearchBar";
-import SearchBarHelp from "./SearchBarHelp";
 import styles from "../css/searchBarSection.module.css";
 
 function SearchBarSection() {
   return (
     <div className={styles.searchContainer}>
       <SearcBar />
-      <SearchBarHelp />
     </div>
   );
 }
