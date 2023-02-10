3ad8423176e43c6176e74edd61bf747ade121bce
leotricotti <tricottileo@gmail.com>
2023-02-10 15:43:34 -0300

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,19 +5,25 @@ import styles from "../css/card.module.css";
 
 function Card({ dataFromApi, backgroundColor, onClose }) {
   return (
-    <div
-      className={styles.cardContainer}
-      style={{
-        backgroundColor: `var(${backgroundColor})`,
-      }}
-    >
-      <button className={styles.closeBtn} onClick={onClose}>
-        <span className={styles.closeIcon}>x</span>
-      </button>
-      <CardHeader dataFromApi={dataFromApi} backgroundColor={backgroundColor} />
-      <CardBody dataFromApi={dataFromApi} />
-      <ModeSwitcherMobile />
-    </div>
+    <>
+      <div className={styles.overlayBlack}></div>
+      <div
+        className={styles.cardContainer}
+        style={{
+          backgroundColor: `var(${backgroundColor})`,
+        }}
+      >
+        <button className={styles.closeBtn} onClick={onClose}>
+          <span className={styles.closeIcon}>x</span>
+        </button>
+        <CardHeader
+          dataFromApi={dataFromApi}
+          backgroundColor={backgroundColor}
+        />
+        <CardBody dataFromApi={dataFromApi} />
+        <ModeSwitcherMobile />
+      </div>
+    </>
   );
 }
 
