cd4198ccee6defc59597697a9e1c351bee60cd70
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-10 08:58:50 -0300

    commit

--- src/components/SearchBarHelp.js
+++ src/components/SearchBarHelp.js
@@ -4,6 +4,11 @@ function SearchBarHelp() {
   return (
     <div className={styles.helpContainer}>
       <div className={styles.helpIcon}></div>
+      <h3 className={styles.title}>Search by name</h3>
+      <p className={styles.text}>
+        Type a compound name in the search field. If the compound is found, the
+        App will display its component.
+      </p>
     </div>
   );
 }
