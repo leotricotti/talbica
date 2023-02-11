df0ac2e8b76dbf1d3f9ed0ba32cfe556dc70fcfe
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-11 08:51:11 -0300

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,50 @@
+import { CSSTransition } from "react-transition-group";
+import { useState, useRef, useEffect } from "react";
 import CardHeader from "./CardHeader";
 import CardBody from "./CarBody";
 import ModeSwitcherMobile from "./ModeSwitcherMobile";
 import styles from "../css/card.module.css";
 
 function Card({ dataFromApi, backgroundColor, onClose, selectedElement }) {
+  const [showCard, setShowCard] = useState(false);
+  const nodeRef = useRef(null);
+
+  useEffect(() => {
+    setShowCard(true);
+  }, [selectedElement]);
+
   return (
     <>
       <div className={styles.overlayBlack}></div>
-      <div
-        className={`${styles.cardContainer} ${
-          selectedElement ? styles.active : ""
-        }`}
-        style={{
-          backgroundColor: `var(${backgroundColor})`,
+      <CSSTransition
+        nodeRef={nodeRef}
+        in={showCard}
+        timeout={5000}
+        classNames={{
+          enter: styles.cardEnter,
+          enterActive: styles.cardEnterActive,
+          enterDone: styles.cardEnterDone,
         }}
       >
-        <button className={styles.closeBtn} onClick={onClose}>
-          <span className={styles.closeIcon}>x</span>
-        </button>
-        <CardHeader
-          dataFromApi={dataFromApi}
-          backgroundColor={backgroundColor}
-          selectedElement={selectedElement}
-        />
-        <CardBody dataFromApi={dataFromApi} />
-        <ModeSwitcherMobile />
-      </div>
+        <div
+          ref={nodeRef}
+          className={styles.cardContainer}
+          style={{
+            backgroundColor: `var(${backgroundColor})`,
+          }}
+        >
+          <button className={styles.closeBtn} onClick={onClose}>
+            <span className={styles.closeIcon}>x</span>
+          </button>
+          <CardHeader
+            dataFromApi={dataFromApi}
+            backgroundColor={backgroundColor}
+            selectedElement={selectedElement}
+          ></CardHeader>
+          <CardBody dataFromApi={dataFromApi} />
+          <ModeSwitcherMobile />
+        </div>
+      </CSSTransition>
     </>
   );
 }
