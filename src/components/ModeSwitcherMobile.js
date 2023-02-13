f005c16f0ffb022bd442727b538c88792611f721
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-12 21:13:14 -0300

    commit

--- src/components/ModeSwitcherMobile.js
+++ src/components/ModeSwitcherMobile.js
@@ -1,10 +1,32 @@
+import React, { useState } from "react";
 import styles from "../css/modeSwitcherMobile.module.css";
 
 function ModeSwitcherMobile() {
+  const [mode, setMode] = useState("info");
+
+  const handleClick = (newMode) => {
+    setMode(newMode);
+  };
+
   return (
     <div className={styles.modeSwitcherContainer}>
-      <button className={styles.modeSwitcherInfo}>Info</button>
-      <button className={styles.modeSwitcherPhoto}>Photo</button>
+      <div
+        className={`${styles.modeSwitcherActive} ${
+          mode === "info" ? styles.infoActive : styles.photoActive
+        }`}
+      ></div>
+      <button
+        className={styles.modeSwitcherInfo}
+        onClick={() => handleClick("info")}
+      >
+        Info
+      </button>
+      <button
+        className={styles.modeSwitcherPhoto}
+        onClick={() => handleClick("photo")}
+      >
+        Photo
+      </button>
     </div>
   );
 }
