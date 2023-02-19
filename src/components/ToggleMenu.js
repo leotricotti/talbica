3aae97af032d83b2b8b3f759c3f752429e7ac7bf
Leonardo Tricotti <tricottileo@gmail.com>
2023-02-18 21:24:45 -0300

    commit

--- src/components/ToggleMenu.js
+++ src/components/ToggleMenu.js
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
 import TableSwitcher from "./TableSwitcher";
 import TableActions from "./TableActions";
 import styles from "../css/toggleMenu.module.css";
 
 function ToggleMenu({ isOpen }) {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  useEffect(() => {
+    setOpenMenu(true);
+    document.body.style.overflow = openMenu ? "hidden" : "auto";
+  }, [isOpen, openMenu]);
+
   return (
-    <div className={`${true ? styles.openToggle : styles.hiddeToggle}`}>
+    <div className={`${openMenu ? styles.openToggle : ""}`}>
       <TableSwitcher />
       <TableActions title={"Heatmaps"} />
     </div>
