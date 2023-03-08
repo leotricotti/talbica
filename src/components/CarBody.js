5dff8158579acd0dfe52a34b6762df3bdbe7e144
Leonardo Tricotti <tricottileo@gmail.com>
2023-03-08 17:14:50 -0300

    commit

--- src/components/CarBody.js
+++ src/components/CarBody.js
@@ -1,64 +1,64 @@
 import styles from "../css/cardBody.module.css";
 
-function CardBody({ dataFromApi }) {
-  const item = dataFromApi;
+function Title({ title }) {
+  return <h3 className={styles.cardBodyTitle}>{title}</h3>;
+}
 
-  const capitalizeChemicalCategory = item.groupBlock.replace(
+function Property({ dataFromApi, title }) {
+  return (
+    <div className={styles.propertyItems}>
+      <span className={styles.propertyTitle}>{title}</span>
+      <span className={styles.propertyValue}>{dataFromApi}</span>
+    </div>
+  );
+}
+
+function PropertySymbol({ dataFromApi, title, symbol }) {
+  return (
+    <div className={styles.propertyItems}>
+      <span className={styles.propertyTitle}>{title}</span>
+      <span className={styles.propertySymbol}>{symbol}</span>
+      <span className={styles.propertyValue}>{dataFromApi}</span>
+    </div>
+  );
+}
+
+function CardBody({ dataFromApi }) {
+  const capitalizeChemicalCategory = dataFromApi.groupBlock.replace(
     /(^\w{1})|(\s+\w{1})/g,
     (letter) => letter.toUpperCase()
   );
 
   return (
-    <div key={item.atomicNumber} className={styles.carBodyContainer}>
-      <h3 className={styles.cardBodyTitle}>Summary</h3>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Symbol</span>
-        <span className={styles.propertyValue}>{item.symbol}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Name</span>
-        <span className={styles.propertyValue}>{item.name}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Chemical category</span>
-        <span className={styles.propertyValue}>
-          {capitalizeChemicalCategory}
-        </span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Atomic number</span>
-        <span className={styles.propertySymbol}>Z</span>
-        <span className={styles.propertyValue}>{item.atomicNumber}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Boiling point</span>
-        <span className={styles.propertyValue}>{item.boilingPoint}</span>
-        <span className={styles.propertySymbol}>F °</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Period</span>
-        <span className={styles.propertyValue}>{item.period}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Group</span>
-        <span className={styles.propertyValue}>{item.group}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Block</span>
-        <span className={styles.propertyValue}>{item.block}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Oxidation states</span>
-        <span className={styles.propertyValue}>{item.osxidationStates}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Color</span>
-        <span className={styles.propertyValue}>{item.color}</span>
-      </div>
-      <div className={styles.propertyItems}>
-        <span className={styles.propertyTitle}>Year discovered</span>
-        <span className={styles.propertyValue}>{item.yearDiscovered}</span>
-      </div>
+    <div key={dataFromApi.atomicNumber} className={styles.carBodyContainer}>
+      <Title title={"Summary"} />
+      <Property title={"Symbol"} dataFromApi={dataFromApi.symbol} />
+      <Property title={"Name"} dataFromApi={dataFromApi.name} />
+      <Property
+        title={"Chemical category"}
+        dataFromApi={capitalizeChemicalCategory}
+      />
+      <PropertySymbol
+        title={"Atomic number"}
+        dataFromApi={dataFromApi.atomicNumber}
+        symbol={"Z"}
+      />
+      <PropertySymbol
+        title={"Boiling point"}
+        dataFromApi={dataFromApi.boilingPoint}
+        symbol={"F°"}
+      />
+      <Property title={"Period"} dataFromApi={dataFromApi.period} />
+      <Property title={"Group"} dataFromApi={dataFromApi.group} />
+      <Property title={"Block"} dataFromApi={dataFromApi.block} />
+      <Property
+        title={"Oxidation states"}
+        dataFromApi={dataFromApi.oxidationStates}
+      />
+      <Property
+        title={"Year discovered"}
+        dataFromApi={dataFromApi.yearDiscovered}
+      />
     </div>
   );
 }
