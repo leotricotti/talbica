97997738db70e6a47b3dbc132a26492239c11b8d
Leonardo Tricotti <tricottileo@gmail.com>
2023-04-21 13:06:39 -0300

    commit

--- src/screens/TableApp.jsx
+++ src/screens/TableApp.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { DataContext } from "../contexts/DataContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 import Spinner from "../components/Spinner";
@@ -21,23 +21,23 @@ function TableApp() {
   const { dataFromApi, error, isLoading } = useContext(DataContext).value;
   const { themeHandler } = useContext(ThemeContext).value;
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth);
-    if (windowWidth > 500) {
-      document.body.style.transform = "scale(.5)";
-    } else {
-      document.body.style.transform = "scale(1)";
-    }
-  };
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  //   if (windowWidth > 500) {
+  //     document.body.style.transform = "scale(.5)";
+  //   } else {
+  //     document.body.style.transform = "scale(1)";
+  //   }
+  // };
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // });
 
   if (error) {
     return (
